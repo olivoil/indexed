d754e410c8296f2daec97b671953fe158aca290b
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-07-26 11:52:56 +0600

    Ran first levelDown suite

--- test/abstract-leveldown-suite.js
+++ test/abstract-leveldown-suite.js
@@ -7,12 +7,12 @@ function factory(location) {
 
 // Test compatibility with basic LevelDOWN API
 
-// require('abstract-leveldown/abstract/leveldown-test').args(factory, tape, testCommon)
+require('abstract-leveldown/abstract/leveldown-test').args(factory, tape, testCommon);
 // require('abstract-leveldown/abstract/open-test').args(factory, tape, testCommon)
 // require('abstract-leveldown/abstract/open-test').open(factory, tape, testCommon)
 // require('abstract-leveldown/abstract/put-test').all(factory, tape, testCommon)
 // require('abstract-leveldown/abstract/del-test').all(factory, tape, testCommon)
-require('abstract-leveldown/abstract/get-test').all(factory, tape, testCommon);
+// require('abstract-leveldown/abstract/get-test').all(factory, tape, testCommon);
 // require('abstract-leveldown/abstract/put-get-del-test').all(factory, tape, testCommon, testBuffer)
 // require('abstract-leveldown/abstract/batch-test').all(factory, tape, testCommon)
 // require('abstract-leveldown/abstract/chained-batch-test').all(factory, tape, testCommon)

bdfbdc34ae5b9c21c4c84960e38c9a056289c06e
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-07-26 13:27:29 +0600

    Added basic implementations of open and close methods

--- test/leveldown/test-suite.js
+++ test/leveldown/test-suite.js
@@ -8,11 +8,11 @@ function factory(location) {
 // Test compatibility with basic LevelDOWN API
 require('abstract-leveldown/abstract/leveldown-test').args(factory, tape, testCommon);
 require('abstract-leveldown/abstract/open-test').all(factory, tape, testCommon);
+require('abstract-leveldown/abstract/close-test').close(factory, tape, testCommon);
 // require('abstract-leveldown/abstract/put-test').all(factory, tape, testCommon)
 // require('abstract-leveldown/abstract/del-test').all(factory, tape, testCommon)
 // require('abstract-leveldown/abstract/get-test').all(factory, tape, testCommon);
 // require('abstract-leveldown/abstract/put-get-del-test').all(factory, tape, testCommon, testBuffer)
 // require('abstract-leveldown/abstract/batch-test').all(factory, tape, testCommon)
 // require('abstract-leveldown/abstract/chained-batch-test').all(factory, tape, testCommon)
-// require('abstract-leveldown/abstract/close-test').close(factory, tape, testCommon)
 // require('abstract-leveldown/abstract/iterator-test').all(factory, tape, testCommon)

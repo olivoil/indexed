ce6583386a73a42b993cc4bb07ebedd21963d91d
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-07-26 16:31:13 +0600

    Finished test suite for put

--- test/leveldown/test-suite.js
+++ test/leveldown/test-suite.js
@@ -9,7 +9,7 @@ function factory(location) {
 require('abstract-leveldown/abstract/leveldown-test').args(factory, tape, testCommon);
 require('abstract-leveldown/abstract/open-test').all(factory, tape, testCommon);
 require('abstract-leveldown/abstract/close-test').close(factory, tape, testCommon);
-// require('abstract-leveldown/abstract/put-test').all(factory, tape, testCommon)
+require('abstract-leveldown/abstract/put-test').all(factory, tape, testCommon);
 // require('abstract-leveldown/abstract/del-test').all(factory, tape, testCommon)
 // require('abstract-leveldown/abstract/get-test').all(factory, tape, testCommon);
 // require('abstract-leveldown/abstract/put-get-del-test').all(factory, tape, testCommon, testBuffer)

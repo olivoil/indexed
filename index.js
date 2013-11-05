6b6b6be15a7869703adda91f67f02ef7ae63cd92
Olivier Melcher <olivier.melcher@gmail.com>
2013-11-05 18:58:28 -0500

    create index only when updgrade needed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var Indexed  = require('./lib/indexeddb');
-module.exports = Indexed.supported ? Indexed : require('./lib/localstorage');
+var LocalStorage  = require('./lib/localstorage');
+
+var supported = Indexed.supported || LocalStorage.supported;
+
+if(supported){
+  exports = module.exports = Indexed.supported ? Indexed : LocalStorage;
+} else {
+}
+
+exports.supported = supported;
